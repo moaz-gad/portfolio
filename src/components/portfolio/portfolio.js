0a81a41f9ae27dabc9e01d1a40a7d8a6d7a1e482
MOAZ GAD <moaz.a.gad@gmail.com>
2022-08-10 23:04:00 +0200

    august update

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,27 +1,50 @@
-import React from "react";
-import "./portfolio.css";
-import IMG1 from "../../assets/bootstrap-project.png";
-import IMG2 from "../../assets/mansoug-website.png";
-import IMG3 from "../../assets/memory-game.png";
-import IMG4 from "../../assets/maven.png";
-import IMG5 from "../../assets/portfolio.png";
-import IMG6 from "../../assets/souqgomaa.png";
-import IMG7 from "../../assets/maady.png";
+import React from "react"
+import "./portfolio.css"
+import IMG1 from "../../assets/bootstrap-project.png"
+import IMG2 from "../../assets/mansoug-website.png"
+import IMG3 from "../../assets/memory-game.png"
+import IMG4 from "../../assets/maven.png"
+import IMG5 from "../../assets/portfolio.png"
+import IMG6 from "../../assets/souqgomaa.png"
+import IMG7 from "../../assets/maady.png"
+import IMG8 from "../../assets/experts.png"
+import IMG9 from "../../assets/customCatPage.png"
 
 const data = [
-  {
-    id: 4,
-    image: IMG4,
-    title: "Wordpress commercial website for American maven company ",
-    github: "https://github.com",
-    demo: "http://www.mavenexpertsusa.com/",
-  },
   {
     id: 2,
     image: IMG2,
     title: "Wordpress E-commerce website for mansoug company",
     github: "#portfolio",
     demo: "https://mansoug.com/",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Large E-commerce website Laravel| Bootstrap| CSS| javaScript ",
+    github: "#",
+    demo: "https://souqelgomaa.com/",
+  },
+  {
+    id: 7,
+    image: IMG7,
+    title: "Saudi office for legal matters |WordPress",
+    github: "#",
+    demo: "https://moazz.pkbs.org/",
+  },
+  {
+    id: 8,
+    image: IMG8,
+    title: "Experts company for large projects |WordPress |Elementor",
+    github: "#",
+    demo: "https://co.khksa.com/",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Wordpress commercial website for American maven company ",
+    github: "https://github.com",
+    demo: "http://www.mavenexpertsusa.com/",
   },
   {
     id: 3,
@@ -45,20 +68,13 @@ const data = [
     demo: "https://moaz-gad.github.io/portfolio",
   },
   {
-    id: 6,
-    image: IMG6,
-    title: "Large E-commerce website Laravel| Bootstrap ",
+    id: 9,
+    image: IMG9,
+    title: "Custom category page Wordpress| woocommerce |CSS",
     github: "#",
-    demo: "https://souqelgomaa.com/",
+    demo: "https://w.souqelgomaa.com/product-category/%D8%A3%D8%AC%D9%87%D8%B2%D8%A9-%D9%85%D9%86%D8%B2%D9%84%D9%8A%D8%A9/",
   },
-  {
-    id: 7,
-    image: IMG7,
-    title: "Saudi office for legal matters |WordPress",
-    github: "#",
-    demo: "https://moazz.pkbs.org/",
-  },
-];
+]
 
 const Portfolio = () => {
   return (
@@ -77,15 +93,20 @@ const Portfolio = () => {
                 <a className="btn" href={github}>
                   GitHUb
                 </a>
-                <a className="btn btn-primary" href={demo} target="_blank">
+                <a
+                  className="btn btn-primary"
+                  href={demo}
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   Live Demo
                 </a>
               </div>
             </article>
-          );
+          )
         })}
       </div>
     </section>
-  );
-};
-export default Portfolio;
+  )
+}
+export default Portfolio
